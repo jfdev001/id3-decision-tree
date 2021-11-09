9d027cb4d9de0997998655c98a6905224587234e
Jared Frazier <cscidev001@gmail.com>
2021-11-09 15:19:57 -0600

    moved helper functions into tree

--- id3.py
+++ id3.py
@@ -15,22 +15,26 @@ import math
 
 
 class Node:
-    """Node for decision tree."""
-
-    def __init__(self, state, children=None):
+    def __init__(self, attr_ix,  value=None, left_child=None, right_child=None):
         """Define state for Node.
 
-        :param state: State information of the node.
-        :param children: <class 'dict'> mapping non-terminal
-            node sub-states (e.g., outlook = {sunny, overcast, rain})
-            to some next node. If children is None, then node is 
-            terminal.
-
-        TODO: Is this state formulation valid??
+        :param attr_ix: The index corresponding to the attribute
+            for a particular dataset. For example,
+            if a row in a dataset is [0.2 0.12 0.5 0]
+            where the last column is the label, then there are four
+            potential attribute indices here. If the attribute index
+            is -1, then the value corresponds to the decision
+        :param value: Corresponds to the value passed to a node or
+            the decision associated with a node.
+        :param left_child: Pointer to left Node.
+        :param right_child: Pointer to right Node.
         """
 
-        self.state = state
-        self.children = children
+        # Save args
+        self.attr_ix = attr_ix
+        self.value = value
+        self.left_child = left_child
+        self.right_child = right_child
 
 
 class ID3DecisionTree:
@@ -49,13 +53,6 @@ class ID3DecisionTree:
 
         # The root of the tree
         self.root = None
-
-        # # Number of classes
-        # self.labels, self.labels_counts = np.unique(
-        #     self.y, return_counts=True)
-
-        # # Compute entropy of set
-        # self.entropy_of_dataset = self.__entropy(self.labels_counts)
 
     def id3(self,):
         """Create the ID3DecisionTree.
@@ -88,24 +85,133 @@ class ID3DecisionTree:
     def information_gain(self, entropy, expected_information):
         return self.__information_gain(entropy, expected_information)
 
-    def __sort(self,):
-        """Sort training data along each attribute.
+    def matrix_of_attrs_belonging_to_labels(self, attr_label_arr):
+        """Computes matrix matching p_i, n_i, ... for any number of values.
 
-        :param:
+        The notation p_i and n_i is from
+        https://hunch.net/~coms-4771/quinlan.pdf
 
-        :return:
+        :param attr_label_arr: 2D <class 'numpy.ndarray'> where the first column
+            is the attribute and the values that the attribute can take
+            on while the second column is the labels (classification)
+            for each value of the attribute.
+
+        :return: <class 'list'> of <class 'list'>
         """
 
-        pass
+        # Compute the unique discrete values that the attributes can
+        # take on
+        unique_attr_values = np.unique(attr_label_arr[:, 0])
 
-    def __binary_split(self,):
-        """Determine potential binary split point based on attr value changes.
+        # Compute the unique discrete values that the labels can
+        # take on
+        unique_label_values = np.unique(attr_label_arr[:, 1])
 
-        :param:
+        # A matrix with 1 row for each discrete value the attribute
+        # can take on and 1 column for each discrete value the
+        # label can take on. This data structure holds the counts
+        # of each attribute value that matches the discrete label value
+        # Example,
+        # ['overcast' 'rain' 'sunny'] for labels = {0, 1}
+        # outlook [[0, 4], [2, 3], [3, 2]]
+        # This means that the outlook attribute which can take on 3 discrete
+        # values has 0 overcast days that are also labeled 0, while it has
+        # 4 overcast days that are also labeled 1.
+        attr_label_count_matrix = []
 
-        :return:
+        # Iterate through each unique value of the attributes
+        # and count the occurences for which the attribute value
+        # is unique value AND the label is a unique value of that label
+        for unique_attr_value in unique_attr_values:
+            counts = []
+            for unique_label_value in unique_label_values:
+                # Boolean vector... elements are True where condition
+                # holds, False otherwise
+                match_vector = np.logical_and(
+                    attr_label_arr[:, 0] == unique_attr_value,
+                    attr_label_arr[:, 1] == unique_label_value)
+
+                # Non-zero means the instances in which the condition is True
+                match_count = np.count_nonzero(match_vector)
+
+                # Append the value to the counts list
+                counts.append(match_count)
+
+            # Append the counts list to the parent matrix
+            attr_label_count_matrix.append(counts)
+
+        # Resulting matrix
+        return attr_label_count_matrix
+
+    def discretize_continuous_data(self, attr_label_arr, return_bins=False):
+        """Converts continuous attributes to discrete valued array."""
+
+        threshold_indices = self.__get_threshold_indices(attr_label_arr)
+        discretized_arr, bins = self.__get_binned_arrs(
+            attr_label_arr, threshold_indices, return_bins)
+
+        if return_bins:
+            return discretized_arr, bins
+        else:
+            return discretized_arr
+
+    def __get_threshold_indices(self, attr_label_arr):
+        """Returns list of indices where adjacent labels differ.
+
+        Array should be sorted.
         """
-        pass
+
+        # Check sorted
+        if attr_label_arr[0, 0] > attr_label_arr[1, 0]:
+            raise ValueError(':param attr_label_arr: must be sorted')
+
+        threshold_tup_indices = []
+
+        for row in range(len(attr_label_arr)-1):
+            cur_label = attr_label_arr[row, -1]
+            next_label = attr_label_arr[row+1, -1]
+
+            if cur_label != next_label:
+                threshold_tup_indices.append((row, row+1))
+
+        return threshold_tup_indices
+
+    def __get_binned_arrs(self, attr_label_arr, threshold_indices, return_bins):
+        """Use thresholds to bin the sorted data."""
+
+        # Compute the bins
+        bins = []
+        for threshold_ix in threshold_indices:
+            bin_ = (attr_label_arr[threshold_ix[0], 0]
+                    + attr_label_arr[threshold_ix[1], 0]) / 2
+            bins.append(bin_)
+
+        # Discretize the data into binned arrs
+        lst_of_discretized_arrs = []
+        for b in bins:
+            # Binary thresholds
+            discretized_attr_label_arr = attr_label_arr.copy()
+            for ix, row in enumerate(discretized_attr_label_arr):
+
+                # attr > bin_ or can be framed as attr >= bin_
+                if row[0] > b:
+                    discretized_attr_label_arr[ix, 0] = 0
+                else:
+                    discretized_attr_label_arr[ix, 0] = 1
+
+            lst_of_discretized_arrs.append(discretized_attr_label_arr)
+
+        if return_bins:
+            return lst_of_discretized_arrs, bins
+        else:
+            return lst_of_discretized_arrs
+
+    def sort_arr(attr_label_arr):
+        """Sorts the array and keeps labels lined up."""
+
+        sorted_attr_indices = np.argsort(attr_label_arr[:, 0])
+        attr_label_arr = attr_label_arr[sorted_attr_indices, :]
+        return attr_label_arr
 
     def __information_gain(self, entropy, expected_information):
         """Calcluate information gain for split point.
@@ -142,7 +248,6 @@ class ID3DecisionTree:
 
     def __expected_information(self, label_counts, attr_counts):
         """Expected information required for tree with some attribute as node.
-
 
         :param label_counts: <class 'list'> of <class 'int'>
         :param attr_counts: <class 'list'> of <class 'list'> where the 
