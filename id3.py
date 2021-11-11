9ac193016d97c1c6bb32e152f5e14628f9e7ca50
Jared Frazier <cscidev001@gmail.com>
2021-11-10 19:06:47 -0600

    debugging id3

--- id3.py
+++ id3.py
@@ -9,26 +9,29 @@ Description: Module for ID3 Decision Tree.
 Ch. 18 AIMA 3ed: Learning from Examples
 """
 
+import logging
+import datetime
+import sys
 import argparse
 import numpy as np
+import pandas as pd
 import math
 
 
 class Node:
     def __init__(
-            self, learning_set_s=None, variable=None, value=None):
+            self, value=None):
         """Define state for Node.
 
-        :param learning_set_s: <class 'numpy.ndarray'>
-        :param variable:
         :param value:
         """
 
-        self.learning_set_s = learning_set_s
-        self.variable = variable
         self.value = value
-        self.decision = None
+
+        self.attribute = None
+        self.learning_set = None
         self.children = []
+        self.decision = None
 
     def add_child(self, node):
         """Appends a child to the list of children of the node."""
@@ -40,20 +43,30 @@ class Node:
 
         return self.children
 
-    def get_variable(self,):
-        """Returns the variable of the node."""
+    def get_attribute(self,):
+        """Returns the attribute of the node."""
 
-        return self.variable
+        return self.attribute
 
     def get_value(self,):
         """Returns the value of the node."""
 
         return self.value
 
-    def get_subset(self,):
+    def get_labels(self,) -> np.ndarray:
         """Returns the labels for all rows."""
 
-        return self.learning_set_s[:, -1]
+        return self.learning_set[:, -1]
+
+    def set_learning_set(self, learning_set):
+        """Sets the learning set for node."""
+
+        self.learning_set = learning_set
+
+    def set_attribute(self, attribute):
+        """Sets the attribute for node."""
+
+        self.attribute = attribute
 
     def set_unanimous_decision(self):
         """Sets unambiguous decision for node."""
@@ -65,17 +78,17 @@ class Node:
 
         self.decision = self.__get_probabilistic_decision()
 
-    def is_root(self,):
+    def is_root(self,) -> bool:
         """Returns bool for whether node is root node.
 
-        A root node WILL have a learning_set_s, it COULD have a decision,
-        it COULD have children, but it should NOT have any variable
+        A root node WILL have a learning_set, it COULD have a decision,
+        it COULD have children, but it should NOT have any attribute
         or value associated with it.
         """
 
-        return self.variable is None and self.value is None
+        return self.attribute is None and self.value is None
 
-    def is_leaf(self,):
+    def is_leaf(self,) -> bool:
         """Returns bool for whether node is leaf.
 
         A leaf node necessarily has no children associated with it.
@@ -87,7 +100,7 @@ class Node:
         """Returns a single element of the label subset if all labels are equal."""
 
         if self.__all_labels_equal():
-            return self.get_subset()[0]
+            return self.get_labels()[0]
 
         else:
             raise ValueError('All labels are NOT equal!')
@@ -96,10 +109,10 @@ class Node:
         """Returns best possible decision for node."""
         return
 
-    def __all_labels_equal(self,):
+    def __all_labels_equal(self,) -> bool:
         """Check to see if all labels in learning set are the same."""
 
-        return np.all(self.get_subset() == self.get_subset[0])
+        return np.all(self.get_labels() == self.get_labels()[0])
 
 
 class ID3DecisionTree:
@@ -111,26 +124,41 @@ class ID3DecisionTree:
         # The root of the tree
         self.root = Node()
 
-    def decision_tree_learning(self, data: np.ndarray):
+    def decision_tree_learning(self, learning_set: np.ndarray) -> None:
         """Helper function for ID3 decision tree learning.
 
         :param data: Contains the learning set and attribute sets.
 
         :return: None
         """
-        self.root.learning_set_s = data
-        self.root = self.__id3(node=self.root)
 
-    def __id3(self, node: Node):
+        self.__id3(learning_set=learning_set, node=self.root)
+
+    def __id3(self, learning_set: np.ndarray, node: Node) -> None:
         """Create the ID3DecisionTree.
 
-        :param data:
+        :param learning_set: The set containing all variables 
+            (features and labels). The learning_set must only have a 
+            single label. The label MUST be the last element in a given
+            row, i.e., the final (-1) column for all rows in the set.
 
-        :return:
+        :return: A tree node.
         """
 
+        # Sort the learning set if there is continuous data
+        pass
+
+        # Discretize the data -- >= instead of >
+        pass
+
+        # Add the learning set to the node
+        node.set_learning_set(learning_set=learning_set)
+
+        # Compute the counts of each category of the label
+        label_counts = np.unique(node.get_labels(), return_counts=True)[-1]
+
         # Compute entropy of subset
-        node_subset_entropy = self.__node_subset_entropy(node)
+        learning_set_entropy = self.__entropy(label_counts)
 
         # Entropy is 0 for data, therefore all records
         # have same value for categorical attribute
@@ -138,7 +166,7 @@ class ID3DecisionTree:
         # Meaning if the input to the decision tree is a variable
         # whose variable matches variable of a leaf node
         # then the leaf node yields its decision attribute
-        if node_subset_entropy == 0:
+        if learning_set_entropy == 0:
             node.set_unanimous_decision()
 
             # # Consider how this recurses... it can only return
@@ -152,17 +180,103 @@ class ID3DecisionTree:
         # If not 0, compute information gain
         # for each attribute and find attribute with max IG(S, A)
         # create child node of the root
-        elif node_subset_entropy != 0:
+        elif learning_set_entropy != 0:
 
-            # Compute choose the attribute with the best information gain
-            # Create a node for each of those
-            pass
-        # for each child in node, apply ID3 but with the new
-        # subset of data corresponding to the
+            # List that holds the information gain of each feature
+            info_gain_lst = []
 
-        # Is this necessary? The node is pass by reference?
-        else:
-            return node
+            # For each feature in the learning set, find the number
+            # of the feature that belongs to each of the label
+            # categories, compute the information expected information
+            # gain, compute the actual information gain, then
+            # add that information gain to the list
+            # TODO: Will have to use the discretized_learning_set
+            # instead of the learning_set for the project
+            for feature in range(learning_set.shape[1] - 1):
+
+                # N x M matrix where N is the number of categories
+                # of the feature and M is the number of categories
+                # of the label.
+                subset_counts = self.count_feature_categories_belonging_to_label_categories(
+                    learning_set)
+
+                expected_info_gain = self.__expected_information(
+                    label_counts=label_counts, subset_counts=subset_counts)
+
+                info_gain = self.__information_gain(
+                    entropy=learning_set_entropy,
+                    expected_information=expected_info_gain)
+
+                info_gain_lst.append(info_gain)
+
+            # Get the index of the highest information gain...
+            # this corresponds to the feature with the highest
+            # information gain
+            # TODO: Tie breakers
+            best_feature_ix = np.argmax(info_gain_lst)
+
+            # Should set the attribute of the current node
+            # to this best feature ix per the pseudocode
+            # "Decision Tree Attribute for Root (Node) = A"
+            # from https://en.wikipedia.org/wiki/ID3_algorithm#cite_note-1
+            node.set_attribute(attribute=best_feature_ix)
+
+            # LOGGING
+            LOG.debug('\n')
+            LOG.debug(best_feature_ix)
+
+            # Get the categories corresponding to the attribute (feature)
+            # that had the highest information gain
+            best_feature_categories = np.unique(
+                learning_set[:, best_feature_ix])
+
+            # List of the best learning sets
+            best_category_learning_sets = []
+
+            # Iterate through each of the unique categories for the
+            # best feature and get the learning subsets coresponding
+            # to only that value of the feature...
+            # e.g., For a feature such as
+            # 'outlook = {overcast, sunny, rainy}', the learning subset
+            # containing only one category ('overcast') of the feature
+            # ('outlook') might look like the below array
+            # array(
+            #   [['overcast', 'hot', 'high', False, 'P'],
+            #    ['overcast', 'cool', 'normal', True, 'P'],
+            #    ['overcast', 'mild', 'high', True, 'P'],
+            #    ['overcast', 'hot', 'normal', False, 'P']], dtype=object)
+            for category in best_feature_categories:
+
+                # Uses the indices of the rows for which the feature
+                # category exists to extract the relevant subset (see eg.
+                # if category = 'overcast')...
+                # np.where() returns a tuple, the first element is the
+                # row indices matching the condition. The second element
+                # is not used since it will just be the column
+                # corresponding to the feature.
+                best_category_learning_set = learning_set[
+                    np.where(learning_set == category)[0], :]
+
+                # Append the subset to the list
+                best_category_learning_sets.append(best_category_learning_set)
+
+            # For each of the subsets, create a child node
+            child_nodes = [Node()
+                           for i in range(len(best_category_learning_sets))]
+
+            # Add the child nodes to the current node and call id3
+            for child_node, learning_subset in zip(child_nodes, best_category_learning_sets):
+
+                LOG.debug(child_node)
+                LOG.debug(learning_subset)
+                LOG.debug('\n')
+
+                node.add_child(child_node)
+                self.__id3(learning_set=learning_subset, node=child_node)
+
+        # Returns nothing since the calling function passes the
+        # root node by obj-ref
+        return
 
     def test_tree(self,):
         """Test the ID3DecisionTree.
@@ -185,7 +299,7 @@ class ID3DecisionTree:
     def information_gain(self, entropy: float, expected_information: float) -> float:
         return self.__information_gain(entropy, expected_information)
 
-    def subset_of_attrs_belonging_to_labels(
+    def count_feature_categories_belonging_to_label_categories(
             self,
             attr_label_arr: np.ndarray) -> list[list]:
         """Computes matrix matching p_i, n_i, ... for any number of values.
@@ -378,22 +492,6 @@ class ID3DecisionTree:
             obj_i / sum(obj_counts)) * math.log(obj_i / sum(obj_counts), 2)
             for obj_i in obj_counts if obj_i != 0])
 
-    def __node_subset_entropy(self, node: Node) -> float:
-        """Returns the entropy of the subset (of labels) for node.
-
-        :param node: <class 'Node'>
-
-
-        :return: Entropy(node.get_subset())
-        """
-
-        # Unique counts of labels (e.g., {0, 1} -> {N_0, N_1})
-        _, unique_label_counts = np.unique(
-            node.get_subset(), return_counts=True)
-
-        # Entropy of the subset
-        return self.__entropy(unique_label_counts)
-
     def __expected_information(
             self,
             label_counts: list,
@@ -429,13 +527,35 @@ if __name__ == '__main__':
         help='path to txt file with training data for ID3 decision tree.',
         type=str)
     parser.add_argument(
-        'testing_data',
+        '--testing_data',
         help='path to txt file with testing data for ID3 decision tree.')
     args = parser.parse_args()
 
+    # TODO: Remove Logging
+    global LOG
+    LOG = logging.getLogger()
+    LOG.setLevel(logging.DEBUG)
+    dtime_lst = str(datetime.datetime.now()).split(' ')
+    dtime = dtime_lst[0].replace('-', '') + '_' + \
+        dtime_lst[1].replace(':', '-')[: dtime_lst[1].find('.')]
+    file_out = logging.FileHandler('./logs/' + dtime + '.log')
+    stdout = logging.StreamHandler(sys.stdout)
+    LOG.addHandler(file_out)
+    LOG.addHandler(stdout)
+
     # Tree instantiation
+    tree = ID3DecisionTree()
+
+    # Load learning data
+    learning_set = pd.read_excel(args.training_data).to_numpy()
+
+    print(learning_set)
+
+    # Load testing data
+    testing_set = None
 
     # Train tree
+    tree.decision_tree_learning(learning_set=learning_set)
 
     # Test tree
 
